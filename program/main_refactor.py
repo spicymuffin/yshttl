7b680ecc167bd33697d75ed1c551cc1053ac0473
spicymuffin <54279408+spicymuffin@users.noreply.github.com>
2023-04-30 12:58:35 +0900

    bunch of refactoring

--- program/main_refactor.py
+++ program/main_refactor.py
@@ -1,3 +1,7 @@
+from rich.table import Table
+from rich.panel import Panel
+from rich.columns import Columns
+from urllib.request import urlopen
 import os
 import threading
 import time
@@ -8,11 +12,25 @@ import json
 from rich.console import Console
 import rich
 from rich import print as rprint
+from pprint import pprint
 
-userid = "**replaced USERID using filter-repo**"
-userpw = "**replaced PW using filter-repo**"
+# region file management
+CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+CONFIG_FILE_NAME = "config.json"
+CONFIG_FILE_PATH = CURR_PATH + '\\' + CONFIG_FILE_NAME
+COOKIE_JAR_FILE_NAME = "cookie_jar.txt"
+COOKIE_JAR_FILE_PATH = CURR_PATH + '\\' + COOKIE_JAR_FILE_NAME
+# endregion
+
+USERID = "**replaced USERID using filter-repo**"
+USERPW = "**replaced PW using filter-repo**"
 WMONID = ""
 JSESSIONID = ""
+CLOCK_REFRESH_RATE = 1/4  # in seconds
+BOOK_TIME = datetime.time(13, 55, 0)
+
+CONSOLE = None
+SCHEDULE = None
 
 
 # region utility functions
@@ -40,7 +58,29 @@ def datetime_to_str_time(_datetime):
     return format(_datetime.hour, "0>2") + format(_datetime.minute, "0>2")
 # endregion
 
+# region console i/o
+
+
+def cprint(msg, main=True):
+    s = str(msg)
+    s = s.replace('\n', '\n    ')
+    if main:
+        print(">>> " + s)
+    else:
+        print("\n>>> " + s, flush=True)
+        print("<<< ", end="", flush=True)
+
+
+def cinput(indent=False):
+    if not indent:
+        return input("<<< ")
+    else:
+        return input("    <<< ")
+# endregion
+
 # region classes
+
+
 class WishlistRoute:
     """represent wishlisted routes
     """
@@ -56,9 +96,7 @@ class WishlistRoute:
 
     def __str__(self, verbose=1) -> str:
         if verbose:
-            return f"""  date: {self.departure_datetime.date()}
-  time: {self.departure_datetime.time()}
-origin: {self.origin}"""
+            return f"""date: {self.departure_datetime.date()} time: {self.departure_datetime.time()} origin: {self.origin}"""
         else:
             return str(self.departure_datetime.time())
 
@@ -92,24 +130,32 @@ class Route:
     def __repr__(self):
         return str(self)
 
-    def __str__(self, verbose=1) -> str:
+    def __str__(self, verbose=0) -> str:
         if verbose:
-            return f"""  date: {self.departure_datetime.date()}
-  time: {self.departure_datetime.time()}
-origin: {self.origin}
-remdSt: {self.seats_available}"""
+            return f"""date: {self.departure_datetime.date()} time: {self.departure_datetime.time()} origin: {self.origin} remdSt: {self.seats_available}"""
         else:
             return str(self.departure_datetime.time())
 
 # endregion classes
 
-console = Console()
-console.print("Hello", "World!", style="bold red")
+
+def check_auth_and_exec(func, args):
+    if not server_interface.check_login():
+        cookies = auth_master.get_auth_cookies(USERID, USERPW)
+        if isinstance(cookies, Exception):
+            cprint(f"failed to authenticate: {cookies}")
+            return check_auth_and_exec(func, args)
+        with open(COOKIE_JAR_FILE_PATH, 'w') as file:
+            file.write(cookies[0] + '\n' + cookies[1])
+        WMONID = cookies[0]
+        JSESSIONID = cookies[1]
+        server_interface.WMONID = WMONID
+        server_interface.JSESSIONID = JSESSIONID
+
+    return func(*args)
 
 
-
-
-def get_SHTTL_MAP(_date):
+def get_shttl_map(_date):
     """get shttl map on date _date
 
     Args:
@@ -121,53 +167,137 @@ def get_SHTTL_MAP(_date):
     global WMONID
     global JSESSIONID
 
-    temp_lst_S = server_interface.get_shttl_list("S", _date)
-    temp_lst_I = server_interface.get_shttl_list("I", _date)
+    temp_lst_S = server_interface.get_shttl_list('S', _date)
+    temp_lst_I = server_interface.get_shttl_list('I', _date)
 
-    SHTTL_MAP = {"date": _date, "S": [], "I": []}
+    shttl_map = {'date': _date, "S": [], 'I': []}
 
     for i in range(len(temp_lst_S)):
         rt = Route()
         rt.import_dictionary(temp_lst_S[i])
-        SHTTL_MAP["S"].append(rt)
+        shttl_map['S'].append(rt)
 
     for i in range(len(temp_lst_I)):
         rt = Route()
         rt.import_dictionary(temp_lst_I[i])
-        SHTTL_MAP["I"].append(rt)
+        shttl_map['I'].append(rt)
 
-    # print(SHTTL_DICT)
-
-    return SHTTL_MAP
-
-WMONID, JSESSIONID = auth_master.get_auth_cookies(userid, userpw)
-
-server_interface.WMONID = WMONID
-server_interface.JSESSIONID = JSESSIONID
-
-a = get_SHTTL_MAP("20230427")
-
-rprint(a)
+    return shttl_map
 
 
-import json
-from urllib.request import urlopen
+def get_shttl_map_n_days(_now, n=3):
+    days = []
+    for i in range(n):
+        d = _now + datetime.timedelta(days=i)
+        days.append(datetime_to_str_date(d))
 
-from rich.console import Console
-from rich.columns import Columns
-from rich.panel import Panel
+    maps = []
+    for i in range(n):
+        maps.append(get_shttl_map(days[i]))
 
-
-def get_content(user):
-    """Extract text from user dict."""
-    country = user["location"]["country"]
-    name = f"{user['name']['first']} {user['name']['last']}"
-    return f"[b]{name}[/b]\n[yellow]{country}"
+    return maps
 
 
-console = Console()
+def clock_upd():
+    try:
+        global NOW
+        global DAYS_FROM_START
+        currd = NOW.day
+        while True:
+            time.sleep(CLOCK_REFRESH_RATE)
+            NOW = datetime.datetime.now()
+
+            # force update when 1:55pm happens bc we want to be sneaky and close to 2 pm
+            if not flag and NOW.hour >= BOOK_TIME.hour \
+                    and NOW.minute >= BOOK_TIME.minute \
+                    and NOW.second >= BOOK_TIME.second:
+                rprint("BOOK_TIME")
+                # # update next three days
+                # check_auth_and_exec(update_n3d, (NOW, ))
+                # # check book_queue
+                # check_auth_and_exec(check_book_queue, (NOW, ))
+                # cprint("forced usl, checked bookings", main=False)
+                flag = True
+            if currd < NOW.day:
+                flag = False
+                currd = NOW.day
+                book_schedule_from_sd(DAYS_FROM_START)
+                DAYS_FROM_START += 1
+    except Exception as ex:
+        cprint(f"CRASH! clock_upd_thread dead: {ex}", main=False)
 
 
-users = json.loads(urlopen("https://randomuser.me/api/?results=30").read())["results"]
-user_renderables = [Panel(get_content(user), expand=True) for user in users]
-console.print(Columns(user_renderables))
+def gen_shttl_map_table(_shttl_map):
+    table = Table(title=_shttl_map['date'])
+
+    table.add_column("R. No.", style="cyan", no_wrap=True)
+    table.add_column("Origin", style="green")
+    table.add_column("Time", style="magenta")
+
+    table.add_column("Origin", style="green")
+    table.add_column("Time", style="magenta")
+
+    for i in range(max(len(_shttl_map['S']), len(_shttl_map['I']))):
+        table.add_row(str(i),
+                      'Sinchon' if i < len(_shttl_map['S']) else '-', str(_shttl_map['S'][i].departure_datetime.time()) if i < len(
+            _shttl_map['S']) else '-', 'International' if i < len(_shttl_map['I']) else '-', str(_shttl_map['I'][i].departure_datetime.time()) if i < len(_shttl_map['I']) else '-')
+
+    return table
+
+
+def gen_shttl_map_panels(_shttl_map):
+    S = []
+    I = []
+    for i in range(len(_shttl_map['S'])):
+        S.append(Panel(
+            f"[b]#{i}[/b][yellow] {str(_shttl_map['S'][i].departure_datetime.time())}", expand=True, title_align="right"))
+
+    for i in range(len(_shttl_map['S'])):
+        I.append(Panel(
+            f"[b]#{i}[/b][yellow] {str(_shttl_map['I'][i].departure_datetime.time())}", expand=True, subtitle_align="right"))
+
+    return (Columns(S), Columns(I))
+
+
+def startup():
+    global CONSOLE
+    global SCHEDULE
+
+    # region load cookies and config
+    with open(CONFIG_FILE_PATH, 'r') as file:
+        SCHEDULE = json.load(file)
+
+    if os.path.exists(COOKIE_JAR_FILE_PATH):
+        with open(COOKIE_JAR_FILE_PATH, 'r') as file:
+            WMONID = file.readline()[:-1]  # dont include \n
+            JSESSIONID = file.readline()
+
+    server_interface.WMONID = WMONID
+    server_interface.JSESSIONID = JSESSIONID
+    # endregion
+
+    # check cookie validity
+    if not server_interface.check_login():
+        cookies = auth_master.get_auth_cookies(USERID, USERPW)
+        if isinstance(cookies, Exception):
+            cprint(f"failed to authenticate on startup: {cookies}")
+            return
+        with open(COOKIE_JAR_FILE_PATH, 'w') as file:
+            file.write(cookies[0] + '\n' + cookies[1])
+        WMONID = cookies[0]
+        JSESSIONID = cookies[1]
+        server_interface.WMONID = WMONID
+        server_interface.JSESSIONID = JSESSIONID
+
+    # init console
+    CONSOLE = Console()
+    CONSOLE.print("Hello", "World!", style="bold red")
+
+    maps = get_shttl_map_n_days(datetime.datetime.now(), 4)
+    CONSOLE.print(maps)
+
+    t = gen_shttl_map_table(maps[1])
+    CONSOLE.print(t)
+
+
+startup()
